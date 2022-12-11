45a99b5a7573a0d4f61642bbb6baaed6e2c0b8f4
Diogo <d.magalhaes@ua.pt>
2022-12-11 18:51:04 +0000

    CHEK-55 fluids generator

--- fluids_generator.py
+++ fluids_generator.py
@@ -2,26 +2,38 @@
 import random
 
 
-def generate_fuel(self, fuel, gear, rpm):
-    if gear != 0:
+def generate_fuel(fuel, gear, rpm):
+
+    if gear != 0 and gear != -1:
         fuel -= (rpm / gear  /10000)
+
+    else:
+        fuel -= (rpm / 10000)
+
+
     if fuel <= 0:
         fuel = 0
     
+    
     return fuel
 
-def generate_water(self, water):
+def generate_water(water):
+
     water -= random.uniform(0, 0.01)
+
     if water <= 0:
         water = 0
     
+
     return water
 
 
-def generate_oil(self, oil):
+def generate_oil(oil):
+
     oil -= random.uniform(0, 0.001)
+
     if oil <= 0:
         oil = 0
     
-    return oil
 
+    return oil
